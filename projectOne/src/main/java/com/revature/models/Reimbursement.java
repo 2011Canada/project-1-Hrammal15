2e710e8a75346fb3e1c120d0fa1af2db7254ab10
Hassen Rammal <rammal1@uwindsor.ca>
2020-12-28 17:37:16 -0500

    started doing some work

---
+++ projectOne/src/main/java/com/revature/models/Reimbursement.java
@@ -0,0 +1,153 @@
+package com.revature.models;
+
+public class Reimbursement {
+	
+	 private int id;
+	 private int authorId;
+	 private int resolverId;
+	 private int statusId;  
+	 private int typeId;  
+     private double amount;
+     private String submitted;  
+     private String resolved;
+     private String description;
+     
+     
+     
+	public Reimbursement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Reimbursement(int id, int authorId, int resolverId, int statusId, int typeId, double amount,
+			String submitted, String resolved, String description) {
+		super();
+		this.id = id;
+		this.authorId = authorId;
+		this.resolverId = resolverId;
+		this.statusId = statusId;
+		this.typeId = typeId;
+		this.amount = amount;
+		this.submitted = submitted;
+		this.resolved = resolved;
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Reimbursement [id=" + id + ", authorId=" + authorId + ", resolverId=" + resolverId + ", statusId="
+				+ statusId + ", typeId=" + typeId + ", amount=" + amount + ", submitted=" + submitted + ", resolved="
+				+ resolved + ", description=" + description + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(amount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + authorId;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((resolved == null) ? 0 : resolved.hashCode());
+		result = prime * result + resolverId;
+		result = prime * result + statusId;
+		result = prime * result + ((submitted == null) ? 0 : submitted.hashCode());
+		result = prime * result + typeId;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reimbursement other = (Reimbursement) obj;
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
+			return false;
+		if (authorId != other.authorId)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (resolved == null) {
+			if (other.resolved != null)
+				return false;
+		} else if (!resolved.equals(other.resolved))
+			return false;
+		if (resolverId != other.resolverId)
+			return false;
+		if (statusId != other.statusId)
+			return false;
+		if (submitted == null) {
+			if (other.submitted != null)
+				return false;
+		} else if (!submitted.equals(other.submitted))
+			return false;
+		if (typeId != other.typeId)
+			return false;
+		return true;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+	public int getResolverId() {
+		return resolverId;
+	}
+	public void setResolverId(int resolverId) {
+		this.resolverId = resolverId;
+	}
+	public int getStatusId() {
+		return statusId;
+	}
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
+	public int getTypeId() {
+		return typeId;
+	}
+	public void setTypeId(int typeId) {
+		this.typeId = typeId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getSubmitted() {
+		return submitted;
+	}
+	public void setSubmitted(String submitted) {
+		this.submitted = submitted;
+	}
+	public String getResolved() {
+		return resolved;
+	}
+	public void setResolved(String resolved) {
+		this.resolved = resolved;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+     
+     
+     
+	   
+}
